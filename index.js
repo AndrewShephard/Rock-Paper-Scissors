8ff018cfdd11e9af6b8d223cdee2ff10774a02f3
Andrew Shephard <andrewshephard20@gmail.com>
2023-03-06 00:06:53 -0500

    added new js

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 let computerSelection = ''
 let playerSelection = ''
+let computerScore =0
+let playerScore =0
 let winner
 
 function getComputerChoice() {
     let computerRoll = Math.floor(Math.random() * 3)
-    console.log("Computer Roll: " + computerRoll)
     if (computerRoll == 0) {computerSelection = 'ROCK'}
     else if (computerRoll == 1) {computerSelection = 'PAPER'}
     else {computerSelection = 'SCISSORS'}
-    console.log(computerSelection)
+    return computerSelection;
 };
 
 function playRound(computerSelection, playerSelection){
+    playerSelection = playerSelection.toUpperCase()
     if (playerSelection === computerSelection) {
         winner = 'tie'
       }
@@ -29,6 +31,22 @@ function playRound(computerSelection, playerSelection){
       ) {
         winner = 'computer'
         }
+        return winner;
+    }
+
+function game(){
+ for(let i = 0; i < 5; i++){
+    playRound(computerSelection, "rock")
+    if (winner == 'player') {playerScore++}
+    else if (winner == 'Computer'){computerScore++}
+ }
+ console.log("Computer Score: " + computerScore)
+ console.log("Player Score: " + playerScore)
 }
 
+
 getComputerChoice();
+
+//playRound(computerSelection, "rock");
+
+game()
