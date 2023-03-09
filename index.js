86320be758aee17cff8adaa2783abdd45bb41204
Andrew Shephard <andrewshephard20@gmail.com>
2023-03-08 21:44:50 -0500

    added logic to find winner

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let playerSelection = ''
 let computerScore =0
 let playerScore =0
 let winner
+let winnerMessage =''
 
 function getComputerChoice() {
     let computerRoll = Math.floor(Math.random() * 3)
@@ -35,6 +36,11 @@ function playRound(computerSelection, playerSelection){
         }
     }
 
+    function checkScore(){
+      if (computerScore === 5) {winnerMessage = "Computer Wins";prompt(winnerMessage)}
+      if (playerScore === 5) {winnerMessage = "Player Wins";prompt(winnerMessage)}
+    }
+
     const buttons = document.querySelectorAll('button');
         // we use the .forEach method to iterate through each button
         buttons.forEach((button) => {
@@ -45,7 +51,10 @@ function playRound(computerSelection, playerSelection){
             computerSelection = getComputerChoice();
             playerSelection = e.target.id.toUpperCase();
             playRound(computerSelection, playerSelection);
+            checkScore();
             console.log("Computer: "+ computerSelection + " Player: "+ playerSelection + " Winner:" + winner)
+            console.log("Computer Score: " + computerScore)
+            console.log("Player Score: " + playerScore)
           });
         });
 
